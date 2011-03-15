933bf44b402e9347b8d6cf0c7f8cd1bed9c236d5
Kurper <kurper@gmail.com>
2011-03-14 20:47:12 -0400

    now it gets input and belittles it

--- if2k.js
+++ if2k.js
@@ -1,13 +1,19 @@
-function runif(data) {
+function startif(data) {
     output("poop");
     output("poop");
     output("poop");
 }
+
+function input(text, data) {
+    output(text);
+    output("what a stupid input that was!");
+    return false;
+}
+
 function output(text) {
     $("p#output").append(text + "<br />");
 }
 
-
 $(document).ready(function() {
-    runif(game);
+    startif(game);
 });
